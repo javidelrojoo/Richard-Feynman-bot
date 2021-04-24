67c2ceb7c9b58a5e6d72bbc863b650689cd410b7
javidelrojoo <javitau459@gmail.com>
2021-04-24 19:48:44 -0300

    repito codigo, pero falle en intentar simplificarlo

--- cogs/embed.py
+++ cogs/embed.py
@@ -8,7 +8,9 @@ class Embed(commands.Cog):
     def __init__(self, client):
         self.client = client
     
-    async def get_title_content(self, ctx):
+    @commands.command()
+    async def embed(self, ctx):
+
         def check_author(m):
             return m.author == ctx.message.author
 
@@ -46,12 +48,6 @@ class Embed(commands.Cog):
             await clear_all()
             raise error
             return
-        return title_embed, content_embed
-    
-    @commands.command()
-    async def embed(self, ctx):
-
-        title_embed, content_embed = await self.get_title_content(ctx)
         
         embed=discord.Embed(title=title_embed, description=content_embed)
         await ctx.send(embed=embed)
@@ -69,7 +65,44 @@ class Embed(commands.Cog):
         channel = server.get_channel(channel_id)
         message = await channel.fetch_message(msg_id)
 
-        title_embed, content_embed = await self.get_title_content(ctx)
+        def check_author(m):
+            return m.author == ctx.message.author
+
+        async def clear_all():
+            for i in msg_sent:
+                await i.delete(delay=1.)
+            return
+
+        msg_sent = [ctx.message]
+        
+        msg1 = await ctx.send('¿Que titulo queres para el embed?')
+        msg_sent.append(msg1)
+
+        try:
+            title_embed_msg = await self.client.wait_for("message", check=check_author, timeout=30.)
+            msg_sent.append(title_embed_msg)
+            title_embed = title_embed_msg.content
+        except asyncio.TimeoutError:
+            return await clear_all()
+        except Exception as error:
+            await clear_all()
+            raise error
+            return
+        
+        msg2 = await ctx.send('¿Que contenido queres para el embed?')
+        msg_sent.append(msg2)
+
+        try:
+            content_embed_msg = await self.client.wait_for("message", check=check_author, timeout=30.)
+            msg_sent.append(content_embed_msg)
+            content_embed = content_embed_msg.content
+        except asyncio.TimeoutError:
+            return await clear_all()
+        except Exception as error:
+            await clear_all()
+            raise error
+            return
+        
         embed=discord.Embed(title=title_embed, description=content_embed)
 
         await message.edit(embed=embed)
